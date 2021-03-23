2bd39ffb6c88207c48090dbb72171b36b272c340
Tor Niklas Strøm <tornst@stud.ntnu.no>
2021-03-23 08:52:47 +0100

    updated README.md, got both sockets working

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # socket
 
 for two sockets
+
+## Flow cards 
+
+### Device: Nordpool
+
+#### Triggers
+
+#### Conditions
+
+
+### Release Notes
+
+#### 0.0.1
+- Both sockets can be turned on and off
+
+#### 0.0.1
+- Initial version
--- drivers/socket_driver/device.js
+++ drivers/socket_driver/device.js
@@ -1,13 +1,107 @@
 'use strict';
 
 const { ZigBeeDevice } = require('homey-zigbeedriver');
+// const { ZigBeeDevice } = require('homey-meshdriver');
 const { CLUSTER } = require('zigbee-clusters');
 
 class MyZigBeeDevice extends ZigBeeDevice {
-  onNodeInit() {
-    // Register onoff capability
-    this.registerCapability('onoff', CLUSTER.ON_OFF);
-    console.log("Registered");
-  }
+
+	onNodeInit() {
+		// Register onoff capability
+		// this.registerCapability('onoff', CLUSTER.ON_OFF);
+		
+		
+		// this.registerCapability('onoff.one', CLUSTER.ON_OFF, { endpoint: 1 });
+		// this.registerCapability('onoff.two', CLUSTER.ON_OFF, { endpoint: 2 });
+
+		// this.registerCapability('onoff.one', CLUSTER.ON_OFF, {
+		// 	// This is often just a string, but can be a function as well
+		// 	set: value => (value ? 'setOn' : 'setOff'),
+		// 	  function setParser(setValue) {
+		// 		return setValue ? 'setOn' : 'setOff'; // This could also be an object for more complex
+		// 		// commands
+		// 	  },
+		// 	  get: 'onOff'
+		// 	  report: 'onOff'
+		// 	  function reportParser(report) {
+		// 		if (report && report.onOff === true) return true;
+		// 		return false;
+		// 	  },
+		// 	  reportOpts: {
+		// 		configureAttributeReporting: {
+		// 		  minInterval: 3600, // Minimally once every hour
+		// 		  maxInterval: 60000, // Maximally once every ~16 hours
+		// 		  minChange: 1,
+		// 		}
+		// 	  },
+		// 	  endpoint: 1, // Default is 1
+		// 	  getOpts: {
+		// 		getOnStart: true,
+		// 		getOnOnline: true,
+		// 		pollInterval: 30000, // in ms
+		// 	  }
+		//    })
+
+		this.registerCapability('onoff.one', CLUSTER.ON_OFF, {
+			// This is often just a string, but can be a function as well
+			set: value => (value ? 'setOn' : 'setOff'),
+			setParser: setValue => {
+				return setValue ? 'setOn' : 'setOff'; // This could also be an object for more complex
+				// commands
+			},
+			get: 'onOff',
+			report: 'onOff',
+			reportParser: report => {
+			if (report && report.onOff === true) return true;
+				return false;
+			},
+			reportOpts: {
+				configureAttributeReporting: {
+					minInterval: 3600, // Minimally once every hour
+					maxInterval: 60000, // Maximally once every ~16 hours
+					minChange: 1,
+				}
+			},
+			endpoint: 1, // Default is 1
+			getOpts: {
+				getOnStart: true,
+				getOnOnline: true,
+				pollInterval: 30000, // in ms
+			}
+		})
+		
+		this.registerCapability('onoff.two', CLUSTER.ON_OFF, {
+			// This is often just a string, but can be a function as well
+			set: value => (value ? 'setOn' : 'setOff'),
+			setParser: setValue => {
+				return setValue ? 'setOn' : 'setOff'; // This could also be an object for more complex
+				// commands
+			},
+			get: 'onOff',
+			report: 'onOff',
+			reportParser: report => {
+			if (report && report.onOff === true) return true;
+				return false;
+			},
+			reportOpts: {
+				configureAttributeReporting: {
+					minInterval: 3600, // Minimally once every hour
+					maxInterval: 60000, // Maximally once every ~16 hours
+					minChange: 1,
+				}
+			},
+			endpoint: 2, // Default is 1
+			getOpts: {
+				getOnStart: true,
+				getOnOnline: true,
+				pollInterval: 30000, // in ms
+			}
+		})
+
+		// this.registerCapability('onoff', CLUSTER.ON_OFF, { endpoint: 1 });
+		// this.registerCapability('onoff', CLUSTER.ON_OFF, { endpoint: 2 });
+		
+		console.log("Registered");
+	}
 }
 module.exports = MyZigBeeDevice;
